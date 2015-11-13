cfcfac40cec2d491f8adf94222feab6973bc1116
npklein <niekdeklein@gmail.com>
2015-11-13 13:43:40 +0100

    fastqc with multirows

--- PublicRNAseqParser/parse_output.py
+++ PublicRNAseqParser/parse_output.py
@@ -538,7 +538,8 @@ def parse_fastqc(runinfo_folder_QC,connection,package):
                     img_data = archive.open(name)
                     names.append(description)
                     graphs[description] = connection.add_file(name, description,package+'File', io_stream = img_data,
-                                                              extra_data={'sample_file_id':str(project)+'-'+str(sample_name)+'-'+str(analysis_id)+name.replace(' ','_')})
+                                                              extra_data={'sample_file_id':str(project)+'-'+str(sample_name)+'-'+str(analysis_id)+name.replace(' ','_')},
+                                                              ignore_duplicates=True)
                 if 'fastqc_data.txt' in name:
                     fastqc_data = archive.read(name)
         fastqc_groups = re.search('Filename\s+(\S+)\n'\
@@ -560,51 +561,62 @@ def parse_fastqc(runinfo_folder_QC,connection,package):
                                  +'Adapter Content.*?\n(.*?)\n\>\>END_MODULE.*?'\
                                  +'Kmer Content.*?\n(.*?)\n\>\>END_MODULE', fastqc_data.decode('utf-8'),re.DOTALL)
         fastqc_ids = {'pbsq':'','ptsq':'','psqs':'','pbsc':'','psgc':'','pbnc':'','sld':'','sdl':'','os':'','ac':'','kc':''}
+        to_add = []
         for line in fastqc_groups.group(8).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'base_letter':s_l[0], 'mean':s_l[1], 'median':s_l[2], 'lower_quartile':s_l[3], 'upper_quartile':s_l[4],'percentile_10th':s_l[5], 'percentile_90th':s_l[6]}
-            fastqc_ids['pbsq'] += connection.add_entity_rows(package+'Per_base_sequence_quality',data)+','
+            to_add.append({'base_letter':s_l[0], 'mean':s_l[1], 'median':s_l[2], 'lower_quartile':s_l[3], 'upper_quartile':s_l[4],'percentile_10th':s_l[5], 'percentile_90th':s_l[6]})
+        fastqc_ids['pbsq'] = ','.join(add_multiple_rows(package+'Per_base_sequence_quality',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(9).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'tile':s_l[0], 'base_letter':s_l[1], 'mean':s_l[2]}
-            fastqc_ids['ptsq'] += connection.add_entity_rows(package+'Per_tile_sequence_quality',data)+','
+            to_add.append({'tile':s_l[0], 'base_letter':s_l[1], 'mean':s_l[2]})
+        fastqc_ids['ptsq'] = ','.join(add_multiple_rows(package+'Per_tile_sequence_quality',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(10).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'quality':s_l[0], 'count':s_l[1]}
-            fastqc_ids['psqs'] += connection.add_entity_rows(package+'Per_sequence_quality_scores',data)+','
+            to_add.apped({'quality':s_l[0], 'count':s_l[1]})
+        fastqc_ids['psqs'] = ','.join(add_multiple_rows(package+'Per_sequence_quality_scores',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(11).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'base_letter':s_l[0], 'a':s_l[1], 'g':s_l[2], 'c':s_l[3], 't':s_l[4]}
-            fastqc_ids['pbsc'] += connection.add_entity_rows(package+'Per_base_sequence_content',data)+','
+            to_add.append({'base_letter':s_l[0], 'a':s_l[1], 'g':s_l[2], 'c':s_l[3], 't':s_l[4]})
+        fastqc_ids['pbsc'] = ','.join(add_multiple_rows(package+'Per_base_sequence_content',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(12).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'gc_content':s_l[0], 'count':s_l[1]}
-            fastqc_ids['psgc'] += connection.add_entity_rows(package+'Per_sequence_GC_content',data)+','
+            to_add.append({'gc_content':s_l[0], 'count':s_l[1]})
+        fastqc_ids['psgc'] = ','.join(add_multiple_rows(package+'Per_sequence_GC_content',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(13).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'base_letter':s_l[0], 'n_count':s_l[1]}
-            connection.add_entity_rows(package+'Per_base_N_content',data)
+            to_add.append({'base_letter':s_l[0], 'n_count':s_l[1]})
+        fastqc_ids['pbnc'] = ','.join(add_multiple_rows(package+'Per_base_N_content',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(14).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'length':s_l[0], 'count':s_l[1]}
-            fastqc_ids['sld'] += connection.add_entity_rows(package+'Sequence_length_distribution',data)+','
+            to_add.append({'length':s_l[0], 'count':s_l[1]})
+        fastqc_ids['sld'] = ','.join(add_multiple_rows(package+'Sequence_length_distribution',to_add,connection,True))
         total_deduplicated_perc = fastqc_groups.group(15).split('\n')[0].split('\t')[1]            
+        to_add = []
         for line in fastqc_groups.group(15).split('\n')[2:]:
             s_l = line.split('\t')
-            data = {'duplication_level':s_l[0],'perc_of_deduplicated':s_l[1],'perc_of_total':s_l[2]}
-            fastqc_ids['sdl'] += connection.add_entity_rows(package+'Sequence_duplication_levels',data)+','
+            to_add.append({'duplication_level':s_l[0],'perc_of_deduplicated':s_l[1],'perc_of_total':s_l[2]})
+        fastqc_ids['sdl'] = ','.join(add_multiple_rows(package+'Sequence_duplication_levels',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(16).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'sequence':s_l[0],'count':s_l[1],'percentage':s_l[2],'possible_source':s_l[3]}
-            fastqc_ids['os'] += connection.add_entity_rows(package+'Overrepresented_sequences',data)+','
+            to_add.append({'sequence':s_l[0],'count':s_l[1],'percentage':s_l[2],'possible_source':s_l[3]})
+        fastqc_ids['os'] = ','.join(add_multiple_rows(package+'Overrepresented_sequences',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(17).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'position':s_l[0], 'illumina_universal_adapter':s_l[1], 'illumina_small_rna_adapter':s_l[2], 'nextera_transposase_seq':s_l[3], 'solid_small_rna_adapter':s_l[4]}
-            fastqc_ids['ac'] += connection.add_entity_rows(package+'Adapter_content',data)+','
+            to_add.append({'position':s_l[0], 'illumina_universal_adapter':s_l[1], 'illumina_small_rna_adapter':s_l[2], 'nextera_transposase_seq':s_l[3], 'solid_small_rna_adapter':s_l[4]})
+        fastqc_ids['ac'] = ','.join(add_multiple_rows(package+'Adapter_content',to_add,connection,True))
+        to_add = []
         for line in fastqc_groups.group(18).split('\n')[1:]:
             s_l = line.split('\t')
-            data = {'seq':s_l[0], 'count':s_l[1], 'p_value':s_l[2], 'obs_exp_max':s_l[3], 'max_obs_exp_position':s_l[4]}
-            fastqc_ids['kc'] += connection.add_entity_rows(package+'Kmer_content',data)+','
+            to_add.append({'seq':s_l[0], 'count':s_l[1], 'p_value':s_l[2], 'obs_exp_max':s_l[3], 'max_obs_exp_position':s_l[4]})
+        fastqc_ids['kc'] = ','.join(add_multiple_rows(package+'Kmer_content',to_add,connection,True))
 
         data = {'adapter_content_graph':graphs['adapter_content'][0],'kmer_content_graph':graphs['kmer_profiles'][0],'overrepresented_seqs_graph':graphs['overrepresented_sequences'][0],'per_base_n_content_graph':graphs['per_base_n_content'][0],
                 'per_base_seq_content_graph':graphs['per_base_sequence_content'][0],'per_base_seq_qual_graph':graphs['per_base_quality'][0],'per_seq_GC_content_graph':graphs['per_sequence_gc_content'][0],
